9d0856b5e525abbf8f832d2b83bd88167804f819
Sky-Macbook <parkparkhaneul@gmail.com>
2022-05-10 15:47:26 -0700

    more on model, added differences stats to the model and tried estimating the impact of the kill difference, was not useful.

--- r test 1.R
+++ r test 1.R
@@ -22,7 +22,7 @@ plot(league_data$blueKills~league_data$blueWardsPlaced)
 
 
 league_data$blueDeaths
-league_data$blueKills
+league_data$blueDragons
 
 # Treaming Data
 blue_team_stats <- league_data[,2:21] #including game id colum , (identifier)
@@ -41,7 +41,7 @@ ggpairs(blue_team_stats_first_100)
 ## total gold and gold per min corr : 1.00
 ## total cs and cs per min :1.00
 
-## differences, omitted for not, but can be a stat indicating the cs difference
+## differences, omitted for now, but can be a stat indicating the cs difference
 
 ## elite monster = herald + dragons, keep each, noe the elite
 
@@ -81,9 +81,38 @@ blue_team_stats_dropped = subset(blue_team_stats, select = -c(blueEliteMonsters,
 model2 <- glm( blueWins ~., data = blue_team_stats_dropped, family = binomial)
 model2
 
+blue_team_stats_dropped_3 = subset(blue_team_stats, select = -c(blueEliteMonsters,blueTotalGold,
+                                                                  blueTotalExperience,
+                                                                  blueCSPerMin,
+                                                                  blueGoldPerMin) )
+model3 <-glm( blueWins ~., data = blue_team_stats_dropped_3, family = binomial)
+
+summary(model3)
+
+blue_team_stats_killDiff <- blue_team_stats
+killdiff <- league_data$blueKills - league_data$redKills
+blue_team_stats_killDiff['killDiff'] <- killdiff
+
+blue_team_stats_killDiff$killDiff
+  
+
+blue_team_stats_killDiff_dropped = subset(blue_team_stats_killDiff, select = -c(blueEliteMonsters,blueTotalGold,
+                                                                blueTotalExperience,
+                                                                blueCSPerMin,
+                                                                blueGoldPerMin,
+                                                                blueKills,
+                                                                blueDeaths) )
+
+model4 <-glm( blueWins ~., data = blue_team_stats_killDiff_dropped, family = binomial)
+summary(model4)
+
+## killdiff not really significant
+
 ## conclusion, drag is more associated compared with other achivements
 
-model
+## correlation 1
+
+cor(blue_team_stats_killDiff_dropped$blueKills-blue_team_stats_killDiff_dropped$blueDeaths,blue_team_stats_killDiff_dropped$killDiff)
 
 
 ### PART 2: Statistical Estimation
