5db27b568d76846333011851bb7ebc4a4715a962
Sky-Macbook <parkparkhaneul@gmail.com>
2022-05-10 21:55:26 -0700

    exploring more... assessing models

--- r test 1.R
+++ r test 1.R
@@ -1,6 +1,6 @@
 ### League of Legend 10 K Diamond rank game data analysis using linear regression and estimation methods. 
 
-# purpose is to find the relationship between multiple factors and the win rate(logistic) and kills(for rn warmup)
+# purpose is to find the relationship between multiple factors and the win rate(logistic regression) 
 
 # Data Sourses 
 # https://www.kaggle.com/datasets/bobbyscience/league-of-legends-diamond-ranked-games-10-min?resource=download
@@ -17,83 +17,48 @@ library(GGally)
 league_data
 str(league_data)
 
-plot(league_data$blueKills~league_data$blueWardsPlaced)
+# Treaming the Data
+blue_team_stats <- league_data[,2:21] 
 
-
-
-league_data$blueDeaths
-league_data$blueDragons
-
-# Treaming Data
-blue_team_stats <- league_data[,2:21] #including game id colum , (identifier)
-blue_team_stats_first_100 <- blue_team_stats[0:100,]
-blue_team_stats_first_100
 str(blue_team_stats)
 
-
-
-# check for correlation and get rid of the high correlation (causeing singular matrix)
+# check for correlation and get rid of the high correlation (causing nearly singular matrix)
+blue_team_stats_first_100 <- blue_team_stats[0:100,]
+blue_team_stats_first_100
 ggpairs(blue_team_stats_first_100)
 
+## dropping highly correlated variables
+blue_team_stats_dropped = subset(blue_team_stats, select = 
+                                   -c(blueEliteMonsters,
+                                      blueTotalGold,
+                                      blueTotalExperience,
+                                      blueGoldDiff,
+                                      blueExperienceDiff,
+                                      blueCSPerMin,
+                                      blueGoldPerMin) )
 
-# data got rid of due to high correlation
+logistic_1 <- glm( blueWins ~., data = blue_team_stats_dropped, family = binomial)
+summary(logistic_1)
 
-## total gold and gold per min corr : 1.00
-## total cs and cs per min :1.00
+## importance of the difference
+blue_team_stats_dropped_diff = subset(blue_team_stats, select = -c(blueEliteMonsters,
+                                                                blueTotalGold,
+                                                                blueTotalExperience,
+                                                                blueCSPerMin,
+                                                                blueGoldPerMin) )
+logistic_2 <-glm( blueWins ~., data = blue_team_stats_dropped_diff, family = binomial)
 
-## differences, omitted for now, but can be a stat indicating the cs difference
-
-## elite monster = herald + dragons, keep each, noe the elite
-
-## total gold, could be indicated by kills, minions, etc. 
-
-## total experience ~ avg level, only kepp the avg level because it is what effects the performance, one stronger stat
-
-## dropping such variables
-blue_team_stats_first_100_dropped = subset(blue_team_stats_first_100, select = -c(blueEliteMonsters,blueTotalGold,
-                                                   blueTotalExperience,blueGoldDiff,
-                                                   blueExperienceDiff,blueCSPerMin,
-                                                   blueGoldPerMin) )
-
-blue_team_stats_first_100_dropped
-
-model <- lm(blue_team_stats_first_100_dropped$blueKills~blue_team_stats_first_100_dropped$)
-
-model <- lm(blueKills~.,blue_team_stats_first_100_dropped)
-
-forward <- regsubsets(blueKills ~., data=blue_team_stats_first_100_dropped, method="exhaustive")
-summary(model)
-
-## logistic regresssion
-
-## shows object related achievements are more likely linked with the winning
-
-model1 <- glm( blueWins ~., data = blue_team_stats_first_100_dropped, family = binomial)
-
-## for the total data
-
-blue_team_stats <- league_data[,2:21] #including game id colum , (identifier)
-blue_team_stats_dropped = subset(blue_team_stats, select = -c(blueEliteMonsters,blueTotalGold,
-                                                                                  blueTotalExperience,blueGoldDiff,
-                                                                                  blueExperienceDiff,blueCSPerMin,
-                                                                                  blueGoldPerMin) )
-
-model2 <- glm( blueWins ~., data = blue_team_stats_dropped, family = binomial)
-model2
-
-blue_team_stats_dropped_3 = subset(blue_team_stats, select = -c(blueEliteMonsters,blueTotalGold,
-                                                                  blueTotalExperience,
-                                                                  blueCSPerMin,
-                                                                  blueGoldPerMin) )
-model3 <-glm( blueWins ~., data = blue_team_stats_dropped_3, family = binomial)
-
-summary(model3)
+summary(logistic_2)
 
 blue_team_stats_killDiff <- blue_team_stats
 killdiff <- league_data$blueKills - league_data$redKills
 blue_team_stats_killDiff['killDiff'] <- killdiff
 
 blue_team_stats_killDiff$killDiff
+
+cor(blue_team_stats_killDiff$blueGoldDiff,blue_team_stats_killDiff$killDiff)#high
+cor(blue_team_stats_killDiff$blueExperienceDiff,blue_team_stats_killDiff$killDiff)#high
+cor(blue_team_stats_killDiff$blueAvgLevel,blue_team_stats_killDiff$blueTotalMinionsKilled)#0.5
   
 
 blue_team_stats_killDiff_dropped = subset(blue_team_stats_killDiff, select = -c(blueEliteMonsters,blueTotalGold,
@@ -106,7 +71,28 @@ blue_team_stats_killDiff_dropped = subset(blue_team_stats_killDiff, select = -c(
 model4 <-glm( blueWins ~., data = blue_team_stats_killDiff_dropped, family = binomial)
 summary(model4)
 
+# high correlation between killDiff and gold diff/exp diff, dropped. 
+blue_team_stats_killDiff_dropped_2 = subset(blue_team_stats_killDiff, select = -c(blueEliteMonsters,blueTotalGold,
+                                                                                blueTotalExperience,
+                                                                                blueCSPerMin,
+                                                                                blueGoldPerMin,
+                                                                                blueKills,
+                                                                                blueDeaths,
+                                                                                blueGoldDiff,
+                                                                                blueExperienceDiff) )
+
+model5 <-glm( blueWins ~., data = blue_team_stats_killDiff_dropped_2, family = binomial)
+summary(model5)
+
+model5$aic
+
+with(summary(logistic_2), 1 - deviance/null.deviance)
+
+model6<-glm( blueWins ~killDiff, data = blue_team_stats_killDiff_dropped_2, family = binomial)
+summary(model6)
 ## killdiff not really significant
+
+# bc the gold gained by kill varies, so not necessarly connected to the win, and more kills can lead to disadvantage(death risky)
 
 ## conclusion, drag is more associated compared with other achivements
 
